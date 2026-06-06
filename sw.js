e41d02fafb2a150e564c945d61b3f3ca6f15f67a
Beardy Goatface <48078803+MaximumJavaCoder@users.noreply.github.com>
2026-06-06 13:27:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'df-dial-v2-dfgrinders-theme-20260606';
+const CACHE = 'df-dial-v2-20260606';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
