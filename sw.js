0d3e77ba5ee9925db41f1873d8258ab3caccef60
Beardy Goatface <48078803+MaximumJavaCoder@users.noreply.github.com>
2026-06-06 20:05:54 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('df-v8').then(c=>c.addAll(['./','index.html','styles.css','app.js','manifest.json','icon-192.png','icon-512.png','dfgrinderslogo.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='df-v9';
+const ASSETS=['./','index.html','styles.css','app.js','manifest.json','icon-192.png','icon-512.png','dfgrinderslogo.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
