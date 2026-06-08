e27801a6506f159c367b6e1655b12d5ad6c63f6a
Beardy Goatface <48078803+MaximumJavaCoder@users.noreply.github.com>
2026-06-07 21:55:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='df-dial-v13';
+const C='df-dial-v14';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.json','icon-192.png','icon-512.png','dfgrinderslogo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
